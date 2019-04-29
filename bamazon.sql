86d0a030bdc379f6b4438a74385d67076609a267
Chenla <chenla24@gmail.com>
2019-04-29 19:59:57 -0400

    Table and Products added

--- bamazon.sql
+++ bamazon.sql
@@ -11,3 +11,17 @@ CREATE TABLE products (
     stock_quantity INTEGER(11) NOT NULL,
     PRIMARY KEY (item_id)
 );
+
+-- INSERT PRODUCT DATA INTO TABLE ROWS --
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Amazon Echo', 'Electronics', 49.99, 500),
+        ('Pressure Cooker', 'Cooking', 69.99, 50),
+        ('Magnetic Car Mount', 'Car Acessories', 7.99, 750),
+        ('Fire HD Tablet', 'Electronics', 79.99, 800),
+        ('San Disk 32GB Memory Card', 'Electronics', 13.21, 50),
+        ('Roomba Vacuum', 'Electronics', 279.00, 200),
+        ('Anker Bluetooth Speaker', 'Electronics', 26.99, 75),
+        ('Fire TV Stick', 'Eletronics', 39.99, 450),    
+        ('23andME DNA Test', 'Misc', 130.99, 500),
+        ('Amazon Cloud Camera', 'Electronics', 119.99, 500);
+    
