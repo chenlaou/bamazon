f3cf27192e3ae17e7ad98ad1567e18e2d0f17224
Chenla <chenla24@gmail.com>
2019-04-29 19:16:29 -0400

    Database Created

---
+++ bamazon.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE FOR BAMAZON --
+CREATE DATABASE Bamazon;
+USE Bamazon;
+
+-- CREATE TABLE FOR PRODUCTS --
+CREATE TABLE products (
+    item_id INT(11) AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INTEGER(11) NOT NULL,
+    PRIMARY KEY (item_id)
+);
