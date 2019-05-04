6f2870e8980744c111e95088116e22ce3c1172d1
Chenla <chenla24@gmail.com>
2019-05-04 09:18:07 -0400

    working app

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# bamazon
+# **AMAZON CLI APP**
+### CREATED BY CHENLA OU [MAY 2019]
+An Amazon-like CLI app storefront using MySQL and Node. The app will take in orders from customers and deplete stock from the store's inventory.
+
+## **OVERVIEW**
+
+This is a basic overview of how this app works with instructions on how to run this in your own terminal.
+
+## **Required NPM Packages**
+```
+MySQL
+Inquirer
+```
+
+* First step - is to install `required NPM packages`.
+* Next, User will run **`bamazonCustomer.js`** in their terminal which will print a list a list of all products including ID Number, Product Name and Price.
+```
+node bamazonCustomer.js
+```
+![Results](/images/list2.png)
+
+* List displayed followed by a prompts for the user.
+![Results](/images/prompt1.png)
+* User should input a `product id` number.
+* User will then be asked to `input quantity`.
+![Results](/images/prompt2.png)
+* If the product qunatity is available user will get `total cost.`
+![Results](/images/total.png)
+* If the product qunatity is not available user will get a `message`.
+![Results](/images/toomuch.png)
+
+* User will then have the option to purchase another item which will restart the app or they can select 'n' to complete their order.
+![Results](/images/no.png)
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,41 +1,114 @@
-
-var inquirer = require('inquirer');
-var mysql = require('mysql');
-// var Table = require('cli-table');
+var inquirer = require("inquirer");
+var mysql = require("mysql");
 
 
+// CREATION CONNECTION INFORMATION FOR SQL DATABASE
 var connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
+  host: "localhost",
+  port: 3306,
 
+  user: "root",
 
-	user: 'root',
-
-	// Your password
-	password: 'password',
-	database: 'Bamazon'
+//  INCLUDE PASSWORD FOR MYSQL AND DATABASE PATH
+  password: "password",
+  database: "Bamazon"
 });
 
-// PRINT LIST OF ITEMS 
-function start(){
-	connection.query('SELECT * FROM Bamazon.Products', function(err, res){
-		if(err) throw err;
-		console.log(' ')
-		console.log('------------------------ ★★★★★  WELCOME TO BAMAZON ★★★★★ ---------------------------------------')
-		console.log(' ')
-	
-		for(var i = 0; i<res.length;i++){
-			console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
-			console.log('--------------------------------------------------------------------------------------------------')
-		}
-	})
-};
+// CONNECT TO SQL SERVER/DATABASE
+connection.connect(function(err) {
+  if (err) throw err;
 
-// ASK ID OF PRODUCT FOR USER TO BUY
+  // FUNCTION TO START APP
+  runBamazon();
+});
 
-// ASK HOW MANY UNITS FOR USER TO BUY
+// FUNCTION TO PRINT LIST DISPLAY FOR PURCHASE
+function runBamazon() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    console.log(
+      "--------------------- WELCOME TO BAMAZON! ---------------------"
+    );
+    for (var i = 0; i < res.length; i++) {
+      console.log(
+        "ID: " +
+          res[i].item_id +
+          " : " +
+          "Product: " +
+          res[i].product_name +
+          " - " +
+          "Price: $" +
+          res[i].price
+      );
+    }
+    console.log("---------------------------------------------------------------------------");
 
-// CHECK TO SEE IF PRODUCT QUANITITY IS AVAILABLE 
+    // APP PROMPT TO USER WITH TWO MESSAGES
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "id",
+          message: "What is the ID of the product you would like to buy?"
+        },
+        {
+          type: "input",
+          name: "qty",
+          message: "How many units of this product would you like to buy?"
+        }
+      ])
+      .then(function(answer) {
+        var custProduct = answer.id - 1;
+        var prodQuantity = parseInt(answer.qty);
+        var total = parseFloat(
+          (res[custProduct].price * prodQuantity).toFixed(2)
+        );
 
+        //    Once the customer has placed the order, your application should check if your
+        //    store has enough of the product to meet the customer's request.
+        if (res[custProduct].stock_quantity >= prodQuantity) {
 
-start();
+          // UPDATE SQL DATABSE TO FULFILL ORDER
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: res[custProduct].stock_quantity - prodQuantity
+              },
+              {
+                item_id: answer.id
+              }
+            ],
+            function(err, res) {
+              if (err) throw err;
+              console.log(
+                "Your total is $" + total + ". Thanks for your order!"
+              );
+              reRun();
+            }
+          );
+        } else {
+          console.log("Insufficient quantity");
+          reRun();
+        }
+      });
+  });
+}
+
+// CONFIRM IF CUSTOMER WANTS TO PURCHASE ANOTHER ORDER
+function reRun() {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "reply",
+        message: "Would you like to purchase another item?"
+      }
+    ])
+    .then(function(answer) {
+      if (answer.reply) {
+        runBamazon();
+      } else {
+        console.log("Thanks for visiting BAMAZON!");
+      }
+    });
+}
